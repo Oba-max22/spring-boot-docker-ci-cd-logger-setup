c0a48fd00d8bf178670e81c9646603e31fd2355c
= <=>
2021-06-08 14:33:34 +0100

    slf4j logger

--- src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
+++ src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
@@ -1,14 +1,14 @@
 package com.bezkoder.spring.datajpa;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootDataJpaApplication {
 
-	private static final Logger LOGGER = LogManager.getLogger(SpringBootDataJpaApplication.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootDataJpaApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
 
